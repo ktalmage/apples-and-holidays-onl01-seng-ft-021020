c778801252b18bf5ee9ee779b05834897af755d9
Kyle Talmage <ktalmage85@gmail.com>
2020-02-16 16:36:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -144,17 +144,17 @@ holiday_hash.each do | season, holiday|
   end 
 end
 
-puts all_supplies_in_holidays(holiday_hash)
+
 
 
     
 
 
-# def all_holidays_with_bbq(holiday_hash)
+def all_holidays_with_bbq(holiday_hash)
 #   # return an array of holiday names (as symbols) where supply lists
 #   # include the string "BBQ"
 
-# end
+ end
 
 
 
